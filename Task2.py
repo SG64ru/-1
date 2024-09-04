07c46581215fb1f4ebd98f9caf1055d523025fe5
SG64ru <sergg64@gmail.com>
2024-09-04 22:59:47 +0500

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,6 @@
+#4th program
+print(int((float('123.456'))*10-(int(float('123.456'))*10)))
+print(float('123.456'))
+print(float(('123.456'))*10)
+print(int(float(('123.456'))*10))
+print((int(float(('123.456'))*10))%10)
